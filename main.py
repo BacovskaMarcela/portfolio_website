dcc1aa0dfad6367cb589a512de89fd7728fc71a6
Marcela <bacovska.marcela@gmail.com>
2024-01-10 11:21:58 +0100

    Add description on the new columns

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ with col2:
     st.title("Marcela Bačovská")
     content = """
     Hi, I'm Marcela! I'm an amateur Python enthusiast and data analyst. I've completed a long-term course at Czechitas 
-    called the Digital Academy Data, and I'm currently seeking job opportunities in these fields.
+    called the Digital Academy Data, and I'm currently seeking job opportunities in these fields. 
+    I'm ready to learn anything!
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
